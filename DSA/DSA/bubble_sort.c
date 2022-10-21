e28bd584dbccaeb6428f6dab2461c2126e22eb8f
Mukesh Yadav <mails4ymukesh@gmail.com>
2022-10-21 18:15:01 +0530

    inital setup

---
+++ DSA/DSA/bubble_sort.c
@@ -0,0 +1,12 @@
+//
+//  bubble_sort.c
+//  DSA
+//
+//  Created by Mukesh Yadav on 21/10/22.
+//
+
+#include <stdio.h>
+
+int main() {
+    printf("Ok");
+}
---
+++ DSA/DSA/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  DSA
+//
+//  Created by Mukesh Yadav on 21/10/22.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
